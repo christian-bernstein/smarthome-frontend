c59b3e76d1295234ebf65e6fadd26469acfe2296
Chris <christian.bernsteinde@gmail.com>
2022-10-06 19:17:20 +0200

    unnamed

--- src/smarthome-shiganshina/test/communication/TestCommunication1.ts
+++ src/smarthome-shiganshina/test/communication/TestCommunication1.ts
@@ -1,19 +1,15 @@
 import {CommunicationLayer} from "../../api/communication/CommunicationLayer";
 import {INetInterface} from "../../api/communication/INetInterface";
 import {ICommunicationEndpoint} from "../../api/communication/ICommunicationEndpoint";
+import {ICommunicationLayer} from "../../api/communication/ICommunicationLayer";
 
 export class TestCommunication1 {
 
     constructor() {
-        new CommunicationLayer().endpoints().push(new class implements ICommunicationEndpoint {
-            close(): void {
-
-            }
-
-            id(): string {
-                return "";
-            }
-
+        // Create layer & add new endpoint implementation
+        const cl: ICommunicationLayer = new CommunicationLayer().addEndpoints(new class implements ICommunicationEndpoint {
+            close(): void {}
+            id(): string { return "test-1" }
             netInterface(): INetInterface {
                 return {
                     send(message: string) {
@@ -25,5 +21,12 @@ export class TestCommunication1 {
                 };
             }
         });
+
+        // Add custom on-message interceptor
+        cl.endpointFromID("test-1").netInterface().registerOnMessageReceived(message => {
+
+        });
+
+        // cl.endpointFromID("test-1").transcoder().registerOnTranscodeComplete<PacketType>("packet-type-name", ctx => { .. })
     }
 }
